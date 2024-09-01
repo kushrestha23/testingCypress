f0954271056fb641de16e0b9bf1f8ea463333c2d
Kusumakar Shrestha <kusumakarshrestha@kusumakars-mbp.home>
2024-08-31 20:46:51 -0400

    Added new test cases, cleaned up old ones

--- cypress/e2e/scrape_google.cy.js
+++ cypress/e2e/scrape_google.cy.js
@@ -5,25 +5,12 @@ describe("Google Search Scraping", () => {
     cy.visit("https://www.google.com");
   });
 
-  it("Navigates to Google", () => {
-    cy.visit("https://www.google.com");
-  });
-
-  it("Click on the search bar and type the query", () => {
-    cy.visit("https://www.google.com");
-    cy.slowDown(300);
-
-    //wait for 3 seconds
-    cy.wait(3000);
-
-    //click on the search bar and type and click on the search button
-    const SearchQuery = "Software Development Company in Greater Subury Area";
+  it("Navigates to Google and searches for a query ", () => {
+    cy.get('[name="q"]').should("be.visible");
     cy.get('[name="q"]').type(`${SearchQuery}{enter}`);
-
-    //delaying to stimulate the human-like behavior
     cy.wait(3000);
 
-    cy.get("div.g").each(($el, index, $list) => {
+    cy.get("div.g").each(($el, index) => {
       cy.wrap($el)
         .find("a")
         .invoke("text")
@@ -31,29 +18,26 @@ describe("Google Search Scraping", () => {
           cy.log(`Result ${index + 1}: ${text}`);
         });
     });
+  });
 
-    it("Stores search results in a file", () => {
-      cy.get("div.g").each(($el, index, $list) => {
-        cy.wrap($el)
-          .find("a")
-          .invoke("text")
-          .then((text) => {
-            cy.writeFile(
-              "search_results.txt",
-              `Result ${index + 1}: ${text}\n`,
-              {
-                flag: "a",
-              }
-            );
-          });
-      });
+  it("Stores search results in a file", () => {
+    cy.get('[name="q]').type(`${SearchQuery}{enter}`);
+
+    cy.wait(3000);
+
+    cy.get("div.g").each(($el, index) => {
+      cy.wrap($el)
+        .find("a")
+        .invoke("text")
+        .then((text) => {
+          cy.writeFile(
+            "cypress/search_results.txt",
+            `Results ${index + 1}: ${text}\n`,
+            {
+              flag: "a",
+            }
+          );
+        });
     });
-    //delaying to stimulate the human-like behavior
-    cy.wait(3000);
-
-    //click on the more places button
-
-    cy.contains("More places").click({ multiple: true });
-    cy.wait(3000);
   });
 });
