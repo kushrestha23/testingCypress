f43cd9bd07f06ba7b6cfc71cd4481a43cdbafc81
Kusumakar Shrestha <kusumakarshrestha@Kusumakars-MacBook-Pro.local>
2024-09-01 18:11:05 -0400

    Adding the Cypress Script for scraping the Google and navigates to the google search results page

--- cypress/e2e/scrape_google.cy.js
+++ cypress/e2e/scrape_google.cy.js
@@ -7,30 +7,53 @@ describe("Google Search Scraping", () => {
     cy.get('[name="q"]').type(`${SearchQuery}{enter}`);
     cy.wait(3000);
 
-    cy.get("div.g").each(($el, index) => {
-      cy.wrap($el)
-        .find("a")
-        .invoke("text")
-        .then((text) => {
-          cy.log(`Result ${index + 1}: ${text}`);
-        });
-    });
+    cy.get(".S8ee5").click({ force: true });
 
-    cy.wait(3000);
+    cy.get(".VkpGBb", { timeout: 15000 }).should("be.visible");
 
-    cy.get("div.g").each(($el, index) => {
-      cy.wrap($el)
-        .find("a")
-        .invoke("text")
-        .then((text) => {
-          cy.writeFile(
-            "cypress/search_results.txt",
-            `Results ${index + 1}: ${text}\n`,
-            {
-              flag: "a",
-            }
-          );
-        });
+    cy.get(".VkpGBb").each(($el, index) => {
+      const businessName = $el.find(".dbg0pd").text();
+
+      const address = $el.find(".rllt__wrapped").text();
+
+      const phoneNumber = $el.find(".rllt__details div").eq(2).text();
+
+      // cy.get("div.g").each(($el, index) => {
+      //   cy.wrap($el)
+      //     .find("a")
+      //     .invoke("text")
+      //     .then((text) => {
+      //       cy.log(`Result ${index + 1}: ${text}`);
+      //     });
+      // });
+
+      // cy.wait(3000);
+
+      // cy.get("div.g").each(($el, index) => {
+      //   cy.wrap($el)
+      //     .find("a")
+      //     .invoke("text")
+      //     .then((text) => {
+      //       cy.writeFile(
+      //         "cypress/search_results.txt",
+      //         `Results ${index + 1}: ${text}\n`,
+      //         {
+      //           flag: "a",
+      //         }
+      //       );
+      //     });
+      cy.log(
+        `Business ${
+          index + 1
+        }: ${businessName}, Address: ${address}, Phone: ${phoneNumber}`
+      );
+      cy.writeFile(
+        "cypress/google_directory_results.txt",
+        `Business ${
+          index + 1
+        }: ${businessName}, Address: ${address}, Phone: ${phoneNumber}\n`,
+        { flag: "a" }
+      );
     });
   });
 });
